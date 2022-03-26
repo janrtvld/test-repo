76ebd89c909573323d575bb7121e3cd64f355fbb
janrtvld <60812202+janrtvld@users.noreply.github.com>
2022-03-26 10:15:24 +0000

    Deploying to gh-pages from @ janrtvld/animo-nft-page@75cdd59b87be2022a29c4eef45c836043958588c 🚀

---
+++ _next/static/HHwxMi3b-u5PfFc_WG2vV/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/249-7ab56161854af970.js","static/chunks/pages/index-f66079cf0e206970.js"],"/_error":["static/chunks/pages/_error-1995526792b513b2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
